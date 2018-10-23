f0881bfb281fc2a56f78253569cbb2173bc7180e
toiye <toiye@DESKTOP-BJB9GPG>
2018-10-23 09:10:46 +0700

    Setting environment for firebase web

--- linlishop/src/environments/environment.prod.ts
+++ linlishop/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD85KRTgH18y8iSBSZfTOMjHS910fDLleE",
+    authDomain: "linlishopbg.firebaseapp.com",
+    databaseURL: "https://linlishopbg.firebaseio.com",
+    projectId: "linlishopbg",
+    storageBucket: "linlishopbg.appspot.com",
+    messagingSenderId: "265150089683"
+  }
 };
--- linlishop/src/environments/environment.ts
+++ linlishop/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD85KRTgH18y8iSBSZfTOMjHS910fDLleE",
+    authDomain: "linlishopbg.firebaseapp.com",
+    databaseURL: "https://linlishopbg.firebaseio.com",
+    projectId: "linlishopbg",
+    storageBucket: "linlishopbg.appspot.com",
+    messagingSenderId: "265150089683"
+  }
 };
 
 /*
