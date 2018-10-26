9e5c0df966629bba8303023d3815a2cc68902992
toiye <toiye@DESKTOP-BJB9GPG>
2018-10-26 20:40:30 +0700

    Protect the Router

--- linlishop/src/app/app.module.ts
+++ linlishop/src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { AuthService } from './auth.service';
+import { AuthGuardService } from "./auth-guard.service";
+import { AuthService } from "./auth.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AngularFireModule } from "angularfire2";
@@ -9,19 +10,31 @@ import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { environment } from "src/environments/environment";
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { CheckOutComponent } from './check-out/check-out.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { LoginComponent } from './login/login.component';
-import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from "./home/home.component";
+import { ProductsComponent } from "./products/products.component";
+import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
+import { CheckOutComponent } from "./check-out/check-out.component";
+import { OrderSuccessComponent } from "./order-success/order-success.component";
+import { MyOrdersComponent } from "./my-orders/my-orders.component";
+import { AdminProductsComponent } from "./admin/admin-products/admin-products.component";
+import { AdminOrdersComponent } from "./admin/admin-orders/admin-orders.component";
+import { LoginComponent } from "./login/login.component";
+import { BsNavbarComponent } from "./bs-navbar/bs-navbar.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ProductsComponent, ShoppingCartComponent, CheckOutComponent, OrderSuccessComponent, MyOrdersComponent, AdminProductsComponent, AdminOrdersComponent, LoginComponent, BsNavbarComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent,
+    BsNavbarComponent
+  ],
   imports: [
     NgbCollapseModule,
     NgbModule.forRoot(),
@@ -30,18 +43,38 @@ import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     RouterModule.forRoot([
-      {path: "", component: HomeComponent},
-      {path: "products", component: ProductsComponent},
-      {path: "shopping-cart", component: ShoppingCartComponent},
-      {path: "check-out", component: CheckOutComponent},
-      {path: "my/orders", component: MyOrdersComponent},
-      {path: "order-success", component: OrderSuccessComponent},
-      {path: "login", component: LoginComponent},
-      {path: "admin/products", component: AdminProductsComponent},
-      {path: "admin/orders", component: AdminOrdersComponent}
+      { path: "", component: HomeComponent },
+      { path: "products", component: ProductsComponent },
+      { path: "shopping-cart", component: ShoppingCartComponent },
+      { path: "login", component: LoginComponent },
+      {
+        path: "check-out",
+        component: CheckOutComponent,
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: "my/orders",
+        component: MyOrdersComponent,
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: "order-success",
+        component: OrderSuccessComponent,
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: "admin/products",
+        component: AdminProductsComponent,
+        canActivate: [AuthGuardService]
+      },
+      {
+        path: "admin/orders",
+        component: AdminOrdersComponent,
+        canActivate: [AuthGuardService]
+      }
     ])
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
