c47d52b120b001535ec2ba9c7b6baf8b7266448b
toiye <toiye@DESKTOP-BJB9GPG>
2018-10-26 11:30:56 +0700

    Using Async pipe

--- linlishop/src/app/bs-navbar/bs-navbar.component.ts
+++ linlishop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { AngularFireAuth } from "angularfire2/auth";
 import { Component } from "@angular/core";
 import * as firebase from "firebase";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "bs-navbar",
@@ -9,10 +10,10 @@ import * as firebase from "firebase";
 })
 export class BsNavbarComponent {
   isCollapsed = false;
-  user : firebase.User;
+  user$ : Observable<firebase.User>;
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(user => this.user = user);
+    this.user$ = afAuth.authState;
   }
 
   logout() {
