af7383a268956bd91e2e4d98bee9068e119b3265
toiye <toiye@DESKTOP-BJB9GPG>
2018-10-31 22:17:39 +0700

    Display list of Product

--- linlishop/src/app/admin/admin-products/admin-products.component.ts
+++ linlishop/src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from "./../../product.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-admin-products',
-  templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
+  selector: "app-admin-products",
+  templateUrl: "./admin-products.component.html",
+  styleUrls: ["./admin-products.component.css"]
 })
 export class AdminProductsComponent implements OnInit {
+  products$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private productService: ProductService) {
+    this.products$ = this.productService.getAll();
   }
 
+  ngOnInit() {}
 }
