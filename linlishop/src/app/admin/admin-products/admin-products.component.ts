b5b2fbf34ec5771f6501ccf027146f71604b0a26
toiye <toiye@DESKTOP-BJB9GPG>
2018-11-01 10:29:08 +0700

    Seach products

--- linlishop/src/app/admin/admin-products/admin-products.component.ts
+++ linlishop/src/app/admin/admin-products/admin-products.component.ts
@@ -1,17 +1,38 @@
 import { ProductService } from "./../../product.service";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-admin-products",
   templateUrl: "./admin-products.component.html",
   styleUrls: ["./admin-products.component.css"]
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subsription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subsription = this.productService
+      .getAll()
+      .subscribe(
+        products => (this.filteredProducts = this.products = products)
+      );
   }
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    this.subsription.unsubscribe();
+  }
+
+  filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter(p =>
+          p.payload.node_.children_.root_.right.value.value_
+            .toLowerCase()
+            .includes(query.toLowerCase())
+        )
+      : this.products;
+  }
 }
