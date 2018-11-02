af6bf8fefd3095a33f0f7912f7ea555a55fef0a1
toiye <toiye@DESKTOP-BJB9GPG>
2018-11-02 10:48:27 +0700

    Move home into Product Component

--- linlishop/src/app/app.module.ts
+++ linlishop/src/app/app.module.ts
@@ -55,7 +55,7 @@ import { FormsModule } from "@angular/forms";
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     RouterModule.forRoot([
-      { path: "", component: HomeComponent },
+      { path: "", component: ProductsComponent },
       { path: "products", component: ProductsComponent },
       { path: "shopping-cart", component: ShoppingCartComponent },
       { path: "login", component: LoginComponent },
