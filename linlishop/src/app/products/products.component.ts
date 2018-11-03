423020d2a2d3af860623bd85cd7d9060feb8c5bb
toiye <toiye@DESKTOP-BJB9GPG>
2018-11-03 10:54:53 +0700

    Filter Product from Category

--- linlishop/src/app/products/products.component.ts
+++ linlishop/src/app/products/products.component.ts
@@ -1,18 +1,36 @@
-import { CategoriesService } from './../categories.service';
-import { ProductService } from './../product.service';
-import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { CategoriesService } from "./../categories.service";
+import { ProductService } from "./../product.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"]
 })
 export class ProductsComponent {
-  product$;
+  products = [];
+  filterProducts = [];
   categories$;
+  category: string;
 
-  constructor(productService : ProductService, categoriesService: CategoriesService) { 
-    this.product$ = productService.getAll();
+  constructor(
+    productService: ProductService,
+    categoriesService: CategoriesService,
+    route: ActivatedRoute
+  ) {
+    productService.getAll().subscribe(products => (this.products = products));
     this.categories$ = categoriesService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get("category");
+      this.filterProducts = this.category
+        ? this.products.filter(
+            p =>
+              p.payload.node_.children_.root_.left.left.value.value_ ===
+              this.category
+          )
+        : this.products;
+    });
   }
 }
