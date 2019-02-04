7071922307220085f955f94ce113d060591d6137
Nguyen Ngoc Thanh 20153393 <thanh.nn153393@sis.hust.edu.vn>
2019-02-04 13:59:35 +0700

    Displaying the Quantity

--- linlishop/src/app/products/products.component.ts
+++ linlishop/src/app/products/products.component.ts
@@ -1,20 +1,26 @@
 import { ActivatedRoute } from "@angular/router";
 import { CategoriesService } from "./../categories.service";
 import { ProductService } from "./../product.service";
-import { Component } from "@angular/core";
+import { Component, Input, OnInit, OnDestroy } from "@angular/core";
 import { switchMap } from "rxjs/operators";
 import { ShoppingCartService } from "../shopping-card.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-products",
   templateUrl: "./products.component.html",
   styleUrls: ["./products.component.css"]
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
+  @Input('show-Actions') showActions = true;
+
   products = [];
   filterProducts = [];
   categories$;
   category: string;
+  cart: any;
+  subscription: Subscription;
+
   constructor(
     productService: ProductService,
     categoriesService: CategoriesService,
@@ -39,7 +45,23 @@ export class ProductsComponent {
 
   }
 
+  async ngOnInit() {
+    this.subscription = (await this.cartService.getCart()).valueChanges().subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subscription.unsubscribe();
+  }
+
   addToCart(product) {
     this.cartService.addToCart(product);
+  }
+
+  getQuantity(product) {
+    if (!this.cart) return 0;
+    let item = this.cart.items[product["key"]];
+    return item ? item.quantity : 0;
   }
 }
--- linlishop/src/app/shopping-card.service.ts
+++ linlishop/src/app/shopping-card.service.ts
@@ -15,15 +15,16 @@ export class ShoppingCartService {
     });
   }
 
-  private getCart(cartId: string) {
-    return this.db.object("/shopping-cards/" + cartId);
+  async getCart() {
+    let cartId = await this.getOrCreateCartId();
+    return this.db.object("/shopping-carts/" + cartId);
   }
 
   private getItem(cartId: string, productId: string) {
     return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
   }
 
-  private async getOrCreateCartId() {
+  private async getOrCreateCartId(): Promise<string> {
     let cartId = localStorage.getItem("cartId");
     if (cartId) return cartId;
 
