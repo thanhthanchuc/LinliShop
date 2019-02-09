2a160722be9469db6016010387626530e89b6326
Nguyen Ngoc Thanh 20153393 <thanh.nn153393@sis.hust.edu.vn>
2019-02-09 19:00:58 +0700

    Fixing a fews Design Issue

--- linlishop/src/app/models/shopping-cart.ts
+++ linlishop/src/app/models/shopping-cart.ts
@@ -1,16 +1,16 @@
 import { shoppingCartItem } from "./shoppingCartItem";
 
 export class ShoppingCart {
-    constructor(public items: shoppingCartItem[]) { }
-
-    get productIds() {
-        return Object.keys(this.items);
+    items: shoppingCartItem[] = [];
+    constructor(public itemsMap: { [productId: string]: shoppingCartItem }) {
+        for (let productId in itemsMap)
+            this.items.push(itemsMap[productId]);
     }
 
     get totalItemsCount() {
         let count = 0;
-        for (let productId in this.items)
-            count += this.items[productId].quantity;
+        for (let productId in this.itemsMap)
+            count += this.itemsMap[productId].quantity;
         return count;
     }
 }
